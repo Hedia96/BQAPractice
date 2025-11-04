6c20f607e7f2376a6e4b98d90b603fc5bd931223
Ayatollah Abdelmagied <ayahallah.abdelmagied@vodafone.com>
2025-11-04 13:34:51 +0200

    Pracrice Playwright in java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Playwright playwright = Playwright.create();
+//        Browser browser = playwright.firefox().launch();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        System.out.println(browser.version());
+        page.navigate("https://the-internet.herokuapp.com/javascript_alerts");
+       // playwright.close();
+//
+    }
+}
